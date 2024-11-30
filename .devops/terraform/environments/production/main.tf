1d0f30758d56575eb888a7a4308a0e442e114465
Gabriel Viana <viana_gab2001@hotmail.com>
2024-11-30 05:42:55 -0300

    chore(infra): reduce node of production (#17)

--- .devops/terraform/environments/production/main.tf
+++ .devops/terraform/environments/production/main.tf
@@ -36,7 +36,7 @@ resource "digitalocean_kubernetes_cluster" "main" {
   node_pool {
     name       = "${local.project_name}-${local.environment}-pool"
     size       = "s-1vcpu-2gb"
-    node_count = 3
+    node_count = 2
   }
 }
 
